bceaefb6f1f919313ae30db19470b9858486fe30
holabayor <liasu.olabayo@gmail.com>
2022-09-29 10:38:04 +0100

    fixed pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,18 +23,23 @@ def do_deploy(archive_path):
     filename = archive_path.split('/')[-1]
     if put(archive_path, '/tmp/{}'.format(filename)).failed is True:
         return False
-    if run('mkdir -p /data/web_static/releases/{}'.format(folder)).failed is True:
+    if run('mkdir -p /data/web_static/releases/{}'.format(folder))
+    .failed is True:
         return False
-    if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, folder), warn_only=True).failed is True:
+    if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename,
+                                                                      folder))
+    .failed is True:
         return False
     if run('rm /tmp/{}'.format(filename)).failed is True:
         return False
-    if run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder, folder), warn_only=True).failed  is True:
+    if run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(folder, folder))
+    .failed is True:
         return False
     if run('rm -rf /data/web_static/current').failed is True:
         return False
-    if run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder)).failed is True:
+    if run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(folder)).failed is True:
         return False
     print('New version deployed!')
     return True
-
