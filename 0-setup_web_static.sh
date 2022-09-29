2b3bf806f280758a0ae60d1378eeace8b523637c
holabayor <liasu.olabayo@gmail.com>
2022-09-29 09:44:10 +0100

    fixed commands and requirements

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment of web_static
 
+# Install nginx
+sudo apt update
+sudo apt install -y nginx
+
 # Create folders
-mkdir -p data/web_static/releases/shared/
-mkdir -p data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 # Create a fake HTML file
-sudo echo "Hello World" | sudo tee data/web_static/releases/test/index.html
+sudo echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the folder to the ubuntu user AND group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve content of a dir
-sed -i '44 a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '44 a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # Restart nginx
 sudo service nginx restart
