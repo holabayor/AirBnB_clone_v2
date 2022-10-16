0d1d9dfc8810f2f4d3ce419dbcd18c7779591d67
holabayor <liasu.olabayo@gmail.com>
2022-10-16 21:00:14 +0100

    teardown context

--- 7-states_list.py
+++ 7-states_list.py
@@ -4,39 +4,20 @@ Script that starts a Flask web application
 """
 
 from flask import Flask, render_template
+from models import storage
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def index():
-    return "Hello HBNB!"
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    from models.state import State
+    all_states = storage.all(State)
+    return render_template('7-states_list.html', all_states=all_states)
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    text = text.replace('_', ' ')
-    return 'C %s' % text
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text='is cool'):
-    text = text.replace('_', ' ')
-    return 'Python %s' % text
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def numbers(n):
-    return 'Python %s' % n
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def numbers_template(n):
-    return render_template('5-number.html', n=n)
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def numbers_check(n):
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.teardown_appcontext
+def remove_session(exception):
+    storage.close()
 
 
 if __name__ == '__main__':
